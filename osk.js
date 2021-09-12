768dfc8f736fe4825b68d7839e96005b2b13972a
medverd07 <84049167+medverd07@users.noreply.github.com>
2021-09-12 19:18:05 +0200

    Add files via upload

---
+++ osk.js
@@ -0,0 +1,180 @@
+// ==UserScript==
+// @name         OSK_V 2.0
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://b2c.osk-ins.ru/*
+// @icon         https://www.google.com/s2/favicons?domain=osk-ins.ru
+// @grant        none
+// ==/UserScript==
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function delayedGreeting() {
+
+window.onbeforeunload=null;
+console.log("Поехали...");
+
+await sleep(500)
+
+    try {
+      let vhod = document.querySelector("body > div.wrapper > div.container.content > div > div > div > form > div.ng-scope > div.row > div:nth-child(2) > button").className;
+      if (vhod == 'btn-u pull-right') {console.log("Вход!!!"); document.querySelector("body > div.wrapper > div.container.content > div > div > div > form > div.ng-scope > div.row > div:nth-child(2) > button").click(); };
+    } catch { //  <-- без (err)
+   // ...
+    }
+
+
+await sleep(500)
+
+
+
+for (let a = 0; a < 500; a++) {
+    await sleep(1000)
+    try {
+           let number = document.querySelector("body > div.wrapper > div.container.content > div > div.col-md-9 > div > webslon-wizard > div > div.tab-content > webslon-step:nth-child(1) > div > form > fieldset > div:nth-child(1) > webslon-contractor > div > div:nth-child(6) > section.col.col-3.ng-scope > label.input > input").className;
+      if (number == "ng-pristine ng-untouched ng-valid ng-valid-required ng-valid-mask ng-not-empty") {console.log("Проект загружен!!!"); break}
+    } catch { //  <-- без (err)
+  // ...
+    }
+}
+
+await sleep(1000);
+
+let chck = document.querySelector("body > div.wrapper > div.container.content > div > div.col-md-9 > div > webslon-wizard > div > div.tab-content > webslon-step:nth-child(1) > div > form > fieldset > div:nth-child(4) > section > div > label > input").className;
+if (chck == "ng-pristine ng-untouched ng-empty ng-invalid ng-invalid-required") document.querySelector("body > div.wrapper > div.container.content > div > div.col-md-9 > div > webslon-wizard > div > div.tab-content > webslon-step:nth-child(1) > div > form > fieldset > div:nth-child(4) > section > div > label > i").click();
+
+for (let i = 0; i < 1000; i++) {
+
+    await sleep(500);
+
+try {
+let next = document.querySelector("body > div.wrapper > div.container.content > div > div.col-md-9 > div > webslon-wizard > div > div.wizard-actions > button.btn-next.btn-u").className;
+    if (next == "btn-next btn-u") {await sleep(1000); document.querySelector("body > div.wrapper > div.container.content > div > div.col-md-9 > div > webslon-wizard > div > div.wizard-actions > button.btn-next.btn-u").click(); console.log("Вперед..."); break}
+} catch { //  <-- без (err)
+  // ...
+}
+
+}
+
+await sleep(500);
+
+
+try {
+let chck2 = document.querySelector("body > div.wrapper > div.container.content > div > div.col-md-9 > div > webslon-wizard > div > div.tab-content > webslon-step:nth-child(2) > div > form > fieldset:nth-child(1) > div > section > div > label > input").className;
+if (chck2 == "ng-pristine ng-untouched ng-valid ng-empty") document.querySelector("body > div.wrapper > div.container.content > div > div.col-md-9 > div > webslon-wizard > div > div.tab-content > webslon-step:nth-child(2) > div > form > fieldset:nth-child(1) > div > section > div > label > i").click();
+await sleep(500);
+
+} catch { //  <-- без (err)
+  // ...
+}
+
+for (let i = 0; i < 1000; i++) {
+
+    await sleep(500);
+
+try {
+let next = document.querySelector("body > div.wrapper > div.container.content > div > div.col-md-9 > div > webslon-wizard > div > div.wizard-actions > button.btn-next.btn-u").className;
+    if (next == "btn-next btn-u") {await sleep(1000); document.querySelector("body > div.wrapper > div.container.content > div > div.col-md-9 > div > webslon-wizard > div > div.wizard-actions > button.btn-next.btn-u").click(); console.log("Вперед..."); break}
+} catch { //  <-- без (err)
+  // ...
+}
+
+}
+
+
+for (let i = 0; i < 1000; i++) {
+
+document.querySelector("body > div.wrapper > div.container.content > div > div.col-md-9 > div > webslon-wizard > div > div.tab-content > webslon-step:nth-child(3) > div > form > fieldset > div:nth-child(1) > section.col.col-4 > label.select > select").selectedIndex = 1;
+
+document.querySelector("body > div.wrapper > div.container.content > div > div.col-md-9 > div > webslon-wizard > div > div.tab-content > webslon-step:nth-child(3) > div > form > fieldset > div:nth-child(1) > section.col.col-4 > label.select > select").dispatchEvent(new Event('change'));
+await sleep(500);
+
+document.querySelector("body > div.wrapper > div.container.content > div > div.col-md-9 > div > webslon-wizard > div > div.tab-content > webslon-step:nth-child(3) > div > form > fieldset > div:nth-child(1) > div > section > label.select > select").selectedIndex = 1;
+
+document.querySelector("body > div.wrapper > div.container.content > div > div.col-md-9 > div > webslon-wizard > div > div.tab-content > webslon-step:nth-child(3) > div > form > fieldset > div:nth-child(1) > div > section > label.select > select").dispatchEvent(new Event('change'));
+await sleep(500);
+
+try {
+let next = document.querySelector("body > div.wrapper > div.container.content > div > div.col-md-9 > div > webslon-wizard > div > div.wizard-actions > button.btn-next.btn-u").className;
+    if (next == "btn-next btn-u") {await sleep(1000); document.querySelector("body > div.wrapper > div.container.content > div > div.col-md-9 > div > webslon-wizard > div > div.wizard-actions > button.btn-next.btn-u").click(); console.log("Вперед..."); break}
+} catch { //  <-- без (err)
+  // ...
+}
+
+}
+
+await sleep(500);
+
+document.querySelector("body > div.wrapper > div.container.content > div > div.col-md-9 > div > webslon-wizard > div > div.tab-content > webslon-step:nth-child(4) > div > form > fieldset > section > div > label:nth-child(1) > i").click();
+
+
+for (let i = 0; i < 1000; i++) {
+
+    await sleep(500);
+
+try {
+let next = document.querySelector("body > div.wrapper > div.container.content > div > div.col-md-9 > div > webslon-wizard > div > div.wizard-actions > button.btn-next.btn-u").className;
+    if (next == "btn-next btn-u") {await sleep(1000); document.querySelector("body > div.wrapper > div.container.content > div > div.col-md-9 > div > webslon-wizard > div > div.wizard-actions > button.btn-next.btn-u").click(); console.log("Вперед..."); break}
+} catch { //  <-- без (err)
+  // ...
+}
+
+}
+
+await sleep(500);
+
+document.querySelector("body > div.wrapper > div.container.content > div > div.col-md-9 > div > webslon-wizard > div > div.tab-content > webslon-step:nth-child(5) > div > form > fieldset > div:nth-child(1) > div.row > section:nth-child(1) > label.input.date > input").value = "19.09.2021";
+
+document.querySelector("body > div.wrapper > div.container.content > div > div.col-md-9 > div > webslon-wizard > div > div.tab-content > webslon-step:nth-child(5) > div > form > fieldset > div:nth-child(1) > div.row > section:nth-child(1) > label.input.date > input").dispatchEvent(new Event('change'));
+
+for (let i = 0; i < 1000; i++) {
+
+    await sleep(500);
+
+try {
+let next = document.querySelector("body > div.wrapper > div.container.content > div > div.col-md-9 > div > webslon-wizard > div > div.wizard-actions > button.btn-next.btn-u").className;
+    if (next == "btn-next btn-u") {await sleep(1000); document.querySelector("body > div.wrapper > div.container.content > div > div.col-md-9 > div > webslon-wizard > div > div.wizard-actions > button.btn-next.btn-u").click(); console.log("Вперед..."); break}
+} catch { //  <-- без (err)
+  // ...
+}
+
+}
+
+
+for (let i = 0; i < 1000; i++) {
+
+    await sleep(500);
+
+try {
+let next = document.querySelector("body > div.wrapper > div.container.content > div > div.col-md-9 > div > webslon-wizard > div > div.wizard-actions > button.btn-next.btn-u").className;
+    if (next == "btn-next btn-u") {await sleep(1000); document.querySelector("body > div.wrapper > div.container.content > div > div.col-md-9 > div > webslon-wizard > div > div.wizard-actions > button.btn-next.btn-u").click(); console.log("Вперед...");}
+} catch { //  <-- без (err)
+  // ...
+}
+
+try {
+let chck3 = document.querySelector("body > div.wrapper > div.container.content > div > div.col-md-9 > div > webslon-wizard > div > div.tab-content > webslon-step:nth-child(7) > div > form > fieldset > div > div.tag-box.tag-box-v2.ng-scope > div.ng-scope > section > div > label > input").className;
+if (chck3 == "ng-pristine ng-untouched ng-valid ng-empty") document.querySelector("body > div.wrapper > div.container.content > div > div.col-md-9 > div > webslon-wizard > div > div.tab-content > webslon-step:nth-child(7) > div > form > fieldset > div > div.tag-box.tag-box-v2.ng-scope > div.ng-scope > section > div > label > input").click();
+
+    await sleep(500);
+
+document.querySelector("body > div.wrapper > div.container.content > div > div.col-md-9 > div > webslon-wizard > div > div.tab-content > webslon-step:nth-child(7) > div > form > fieldset > div > div.tag-box.tag-box-v2.ng-scope > div.ng-scope > div:nth-child(4) > p > a > span").click();
+} catch { //  <-- без (err)
+  // ...
+}
+
+
+
+}
+
+
+
+
+
+}
+
+delayedGreeting();
